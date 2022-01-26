bc518898f6b9d8c35bcae5616be2b0df4bc2dd05
Charles K. Neimog <charlesneimog@outlook.com>
2022-01-26 17:39:48 -0300

    add method to loristrck-analysis

--- Sources/Loristrck/loristrck.lisp
+++ Sources/Loristrck/loristrck.lisp
@@ -79,6 +79,14 @@ r w ht hop-o amp fd sl rbw sdif-type minbps minamp fade-time))
     (loop-until-probe-file (outfile outfile))
     (outfile outfile)))
 
+; =========================================
+(defmethod! loristrck-analysis ((sound sound) (outfile pathname) &key (r 60) (w -1) (ht 0) (hop-o 0) (amp -90) (fd -1) (sl -1) (rbw 2000) (sdif-type 'rbep) (minbps 2) (minamp -90) (fade-time 0))
+
+
+(loristrck-analysis (car (list! (if (not (file-pathname sound)) (save-temp-sounds (list sound)) (file-pathname sound))))
+                    (namestring outfile) :r r :w w :ht ht :hop-o hop-o :amp amp :fd fd :sl sl :rbw rbw :sdif-type sdif-type :minbps minbps :minamp minamp :fade-time fade-time))
+
+
 
 ;; ====
 (defmethod! loristrck-analysis ((sound sound) (outfile string) &key (r 60) (w -1) (ht 0) (hop-o 0) (amp -90) (fd -1) (sl -1) (rbw 2000) (sdif-type 'rbep) (minbps 2) (minamp -90) (fade-time 0))
@@ -118,6 +126,11 @@ FADETIME: fade time used when a partial does not end with a 0 amp breakpoint.
 
 (loristrck-analysis (car (list! (if (not (file-pathname sound)) (save-temp-sounds (list sound)) (file-pathname sound))))
                     outfile :r r :w w :ht ht :hop-o hop-o :amp amp :fd fd :sl sl :rbw rbw :sdif-type sdif-type :minbps minbps :minamp minamp :fade-time fade-time))
+
+
+
+
+
 
 ;; ======================== synth sdif ===========================
 
