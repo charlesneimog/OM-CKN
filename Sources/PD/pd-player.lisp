859ff6de42564efa7664f6218a080e7502c63f50
Charles K. Neimog <charlesneimog@outlook.com>
2022-04-23 15:51:18 -0300

    add some comments

--- Sources/PD/pd-player.lisp
+++ Sources/PD/pd-player.lisp
@@ -7,54 +7,53 @@
 
 ;; =============================================================================================================
 
+;; add some preferences
 
 (add-preference-section :externals "PD OSC player" nil '(:SoundFont-Folder :SoundFont :PureData-Player)) 
 (add-preference :externals :PureData-Player "PureData Player" :bool t "If checked, the PureData player will be used to play the score.")
-
 (add-preference :externals :SoundFont-Folder "SoundFound Folder" :folder "Choose a SoundFound Folder")
-
 
 ;; =============================================================================================================
 
+;;; The piece of Code will define the *all-available-soundfonts* to be choosed.
+
 (let* (
       (thepath (get-pref-value :externals :SoundFont-Folder))
-      (thefilelist-sf2 (om::om-directory thepath 
-                                    :type "sf2"
+      (thefilelist-sf2 (om::om-directory thepath  ; "*.sf2"
+                                    :type "sf2" 
                                     :directories t 
                                     :files t 
                                     :resolve-aliases nil 
                                     :hidden-files nil))
       
-      (thefilelist-sf3 (om::om-directory thepath 
+      (thefilelist-sf3 (om::om-directory thepath ; "*.sf3"
                                     :type "sf3"
                                     :directories t 
                                     :files t 
                                     :resolve-aliases nil 
                                     :hidden-files nil))
       
-      (thefilelist (append thefilelist-sf2 thefilelist-sf3))
+      (thefilelist (append thefilelist-sf2 thefilelist-sf3)) ;; append sf2 and sf3
       
       (check_files_inside_folder 
                   (loop :for loop-files :in thefilelist 
                         :collect (if 
                                     (system::directory-pathname-p loop-files)
                                     (search-inside-some-folder loop-files extension)
-                                    loop-files))))
-      (setf *all-available-fonts* (mapcar (lambda (sf2&sf3) (get-filename sf2&sf3)) (remove nil (om::flat check_files_inside_folder)))))
+                                    loop-files)))) ;; Recursive: check sf2 and sf3 inside folders 
 
+      (setf *all-available-soundfonts*  (mapcar (lambda (sf2&sf3) (get-filename sf2&sf3)) (remove nil (om::flat check_files_inside_folder)))))
 
-(add-preference :externals :SoundFont "SoundFound" *all-available-fonts* (car *all-available-fonts*))
+(add-preference :externals :SoundFont "SoundFound" *all-available-soundfonts*  (car *all-available-soundfonts* )) ;; add-preference in OM-Sharp Menu.
+
 ;;; ============================================
 
 (defmethod puredata-player ((voice voice) caller)
 
-
 (if *PureData-PLAY-STATE*
       (let* ()
-                  (om::osc-send (om::osc-msg "/quit-pd" 0) "127.0.0.1" 1996)
+                  (om::osc-send (om::osc-msg "/quit-pd" 0) "127.0.0.1" 1996) ;; kill PD if it is running (Just one process simmultaneously)
                   (setf *PureData-PLAY-STATE* nil))
-
-
 
 (let* ()
             (mp:process-run-function "Open PD"
@@ -64,83 +63,97 @@
                                 (mp:process-run-function "Run PD in Backgroud!"
                                             () 
                                               (lambda () (pd~ 
-                                                            (pd-define-patch "Microtonal-player.pd") 
+                                                            (pd-define-patch "Microtonal-player.pd") ;; Here is the PD patch
                                                                 :var (list (om::x-append 'soundfont (probe-file (om::string+ (om::string+ (get-pref-value :externals :SoundFont-Folder) (get-pref-value :externals :SoundFont))))))
                                                                 :gui nil 
                                                                 :offline nil 
-                                                                :sound-out (tmpfile "casa.wav") 
+                                                                :sound-out (tmpfile "PD.wav") ;; This will not be used just to not need to redefine the pd~ function.
                                                                 :verbose nil))))))
 
             (setf *pd-is-open* nil)
 
-            (om-start-udp-server 3320 "127.0.0.1" (lambda (msg) (let () (if  (equal (car (cdr (osc-decode msg))) 100.0)
+            (om-start-udp-server 3320 "127.0.0.1" (lambda (msg) (let () (if  (equal (car (cdr (osc-decode msg))) 1.0)
                                                                               (let* () (setf *pd-is-open* t) nil)
-                                                                              ))))
+                                                                              )))) ;;; When PD is open, the loadbang will sent one 1 to this port.
 
             (loop :with pd-start = nil 
                   :while (null *pd-is-open*)
-                  :do (sleep 0.01))
+                  :finally (om::om-print "PD is open!" "OM-CKN")) ;; Wait PD to be open!
 
-            (om::om-print "PD is open!" "OM-CKN")
-
-
+            
             (loop :for udp-server :in *running-udp-servers*
                   :do (if (equal (mp:process-name (third udp-server)) "UDP receive server on \"127.0.0.1\" 3320")
-                  (let* () (om::om-stop-udp-server (third udp-server)))))
+                  (let* () (om::om-stop-udp-server (third udp-server))))) ;; Remove the UDP server to check is
+                                                                          ;; the PD is open!
             
             
+            ; This let will send the score data to PureData that will play it!
+            ; For now I am using the sleep function to wait and put it on time. There are some better way??
             (let* (
-                  (score-lonset (lonset voice))
-                  (dx-lonset (om::x-append (car score-lonset) (om::x->dx score-lonset)))
-                  (score-data (mat-trans (list (om::lmidic voice) (om::lvel voice) (om::lchan voice) (om::ldur voice)))))
-                  (setf *PureData-PLAY-STATE* t)
-                  (loop :for onsets :in dx-lonset
+                  (score-lonset (lonset voice)) ;; All onsets of notes
+                  (dx-lonset (om::x-append (car score-lonset) (om::x->dx score-lonset))) ;; if the first figure is a Rest.
+                  (score-data (mat-trans (list (om::lmidic voice) (om::lvel voice) (om::lchan voice) (om::ldur voice)))) ;; organize the data by (note vel chan dur)
+                  (last-duration (car (om::list! (om::ms->sec (car (last (om::ldur voice))))))))
+                  (setf *PureData-PLAY-STATE* t) ; check if there are other voices playing
+                  (loop :for onsets :in dx-lonset ; start the loop
                         :for notes :in score-data 
                         :while *PureData-PLAY-STATE*
                         :do (let* (
-                                    (the-notes (list notes)))
-                                    (sleep (om::ms->sec onsets))
-                                    (mapcar (lambda (x) 
+                                    (the-notes (list notes))) ; the notes to be played
+                                    (sleep (om::ms->sec onsets)) ; wait the time of the onset
+                                    (mapcar (lambda (x) ;; send data already in time to PureData
                                                 (mapcar 
                                                       (lambda (notes lvel lchan ldur) 
                                                             (let* (
-                                                                  (data2send (om::x-append notes lvel lchan ldur))
-                                                                  (format-msg (om::osc-msg "/note" data2send)))
-                                                                  (om::osc-send format-msg "127.0.0.1" 1996))) (first x) (second x) (third x) (fourth x))) the-notes)))
+                                                                  (data2send (om::x-append notes lvel lchan ldur)) ;; last format of data
+                                                                  (format-msg (om::osc-msg "/note" data2send))) ;; create OSC message
+                                                                  (om::osc-send format-msg "127.0.0.1" 1996)))    (first x) ;; note
+                                                                                                                  (second x) ;; vel
+                                                                                                                  (third x) ; lchannel 
+                                                                                                                  (fourth x))) ; duration
+                                                                                                                              the-notes))) ;; send data to PureData
                               
-                        (sleep (ms->sec (car (last dx-lonset))))
-                        (sleep (ms->sec (car (last dx-lonset))))
-                        (box-player-stop caller) ;; fazer ele se atualizar??????????
-                        (if *PureData-PLAY-STATE* (om::osc-send (om::osc-msg "/quit-pd" 0) "127.0.0.1" 1996))
-                        (if *PureData-PLAY-STATE* (setf *PureData-PLAY-STATE* nil))))))
+                        (sleep last-duration) ;; Wait last duration of the voice/chord
+
+                        ;(box-player-stop caller) ;; How to make the timeline (I don't know the name of it) || red bar in the score
+
+                        (if *PureData-PLAY-STATE* (om::osc-send (om::osc-msg "/quit-pd" 0) "127.0.0.1" 1996)) ;; tell PD that the process if finished and kill it
+                        (if *PureData-PLAY-STATE* (setf *PureData-PLAY-STATE* nil)))))) ;; 
       
 
 ;; =====================================================================
-;; Redefinindo o metodo que toca a partitura
+;; Redefine the method that will play the score
 
 (defmethod player-stop-object ((self scheduler) (object score-element))
+  
+  ;; This is OM-Sharp CODE 
+
   (send-current-midi-key-offs object)
   (when (and (equal :auto-bend (get-pref-value :score :microtone-bend))
              *micro-channel-mode-on*)
     (loop for p in (collec-ports-from-object object) do (micro-reset p)))
+
+  ;; Finish of OM-Sharp CODE 
   
   (if (get-pref-value :externals :PureData-Player)
       (let* () 
               (om::om-print "Closing PD" "OM-CKN")
               (om::osc-send (om::osc-msg "/quit-pd" 0) "127.0.0.1" 1996)))
    
-  (call-next-method))
+  (call-next-method)) ;; This is OM-Sharp CODE 
 
 ;; =====================================================================
 
 (defmethod PD-player-play-object ((self scheduler) (object score-element) (caller ScoreBoxEditCall) &key parent interval)
   
-  (declare (ignore parent interval))
+  (declare (ignore parent interval)) ;; This is OM-Sharp CODE 
   
-      (mp:process-run-function "Open PD" () (lambda () (puredata-player object caller))))
+      (mp:process-run-function "Open PD" () (lambda () (puredata-player object caller)))) ;; Open Function to play in PureData
       ;(box-player-start object))
 
 ;;; =======================
+
+;; This is the method used when SPACE in pressed!!
 
 (defmethod play/stop-boxes ((boxlist list))
   (let* ((play-boxes (remove-if-not 'play-box? boxlist)))
