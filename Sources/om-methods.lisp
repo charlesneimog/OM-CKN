6f37d82416fc13ccf5151f9409e0582c4ebd9412
Charles K. Neimog <charlesneimog@outlook.com>
2022-04-24 15:00:23 -0300

    update to add check-update

--- Sources/om-methods.lisp
+++ Sources/om-methods.lisp
@@ -1368,21 +1368,39 @@ Converts a (list of) freq pitch(es) to names of notes."
                                               (print "Closing SonicVisualizer"))))
 
 ;; =======================================================================
-;; Check OM-Sharp version
 
-; (let* (
-;       (tmpfile (om::tmpfile "om-sharp-version.txt"))
-;       (cmd-command
-;             #+windows(om::om-cmd-line (format nil "curl https://raw.githubusercontent.com/charlesneimog/OM-CKN/master/resources/om-sharp.version --ssl-no-revoke --output  ~d" (namestring tmpfile)))
-;             #+unix(om::om-cmd-line (format nil "curl https://raw.githubusercontent.com/charlesneimog/OM-CKN/master/resources/om-sharp.version -L --output ~d" (namestring tmpfile))))
-;       (textfile (uiop:read-file-lines tmpfile)))
-;       (if (> (read-from-string (car textfile)) (read-from-string (format nil "~d.~d" CL-USER::*version-major* CL-USER::*version-minor*)))
-;           (let* (
-;                 (update? (om::om-y-or-n-dialog (format nil "OM-SHARP has been UPDATED to version ~d. Want to update now?" (car textfile)))))
-;                 (if update?
-;                     (let* (
-;                           (sys:open-url "https://github.com/cac-t-u-s/om-sharp/releases/latest"))))))
-;       (alexandria::delete-file tmpfile))
+(defun check-update()
 
+      (with-open-stream 
+            (http (comm:open-tcp-stream 
+                    "raw.githubusercontent.com" 443
+                    :ssl-ctx t
+                    :element-type '(unsigned-byte 8)))
+            (write-sequence (format 
+                        http "GET charlesneimog/OM-CKN/master/resources/om-sharp-version.lisp HTTP/1.1~%Host: raw.githubusercontent.com~%~%" 
+                        (code-char 13) (code-char 10)
+                        (code-char 13) (code-char 10)) http)
+      (force-output http)
+
+      (ignore-errors (loop :for line := (read-line http nil)
+                           :while line 
+                           :do (setf *om-sharp-last-update* line)))
+      (close http)))
+
+;; ========================================================================
+
+
+(setf *actual-version-of-om-sharp* 0)
+(setf *process-of-update* (mp:process-run-function "Check Update for OM-Sharp" () (lambda () (check-update))))
+(sleep 2)
+(mp:process-terminate *process-of-update*)
+(eval (read-from-string *om-sharp-last-update* "OM-Sharp Last Version"))
+
+(if (> *actual-version-of-om-sharp* (read-from-string (format nil "~d.~d" CL-USER::*version-major* CL-USER::*version-minor*)))
+           (let* (
+                 (update? (om::om-y-or-n-dialog (format nil "OM-SHARP has been UPDATED to version ~d. Want to update now?" *actual-version-of-om-sharp*))))
+                 (if update?
+                     (let* ()
+                           (hqn-web:browse "https://github.com/cac-t-u-s/om-sharp/releases/latest")))))
 
       
